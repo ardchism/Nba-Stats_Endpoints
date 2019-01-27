58a975bd9c5d6592735ca3f2875926dd7e2a11d0
ardchism <ardchism@gmail.com>
2019-01-26 20:48:08 -0600

    WIP builderFactory.

--- src/main/java/com/nbastat/player/generators/factories/BuilderFactory.java
+++ src/main/java/com/nbastat/player/generators/factories/BuilderFactory.java
@@ -1,13 +1,23 @@
 package com.nbastat.player.generators.factories;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
 
-import org.jboss.forge.roaster.Roaster;
-import org.jboss.forge.roaster.model.source.JavaClassSource;
+import com.failfast.sourcewriter.domain.FieldAccessLevel;
+import com.failfast.sourcewriter.domain.MethodAccessLevel;
+import com.failfast.sourcewriter.domain.SourceCode;
+import com.failfast.sourcewriter.domain.SourceCode.SourceCodeBuilder;
+import com.failfast.sourcewriter.domain.SourceCodeField.SourceCodeFieldBuilder;
+import com.failfast.sourcewriter.domain.SourceCodeMethod;
+import com.failfast.sourcewriter.domain.SourceCodeMethod.SourceCodeMethodBuilder;
+import com.failfast.sourcewriter.domain.SourceCodeField;
 
 public class BuilderFactory {
 
@@ -20,64 +30,84 @@ public class BuilderFactory {
 
 		Map<String, String> builders = new HashMap<>();
 
+		List<SourceCodeField> sourceCodeFields = new ArrayList<>();
+		List<SourceCodeMethod> sourceCodeMethods = new ArrayList<>();
+		
 		String packageName = currentClass.getPackage().getName() + ".builders";
 		String className = currentClass.getSimpleName() + "Builder";
-
-		final JavaClassSource javaClass = Roaster.create(JavaClassSource.class);
-
-		javaClass.setPackage(packageName).setName(className);
-		javaClass.addImport(currentClass);
-		javaClass.addProperty(currentClass, "builtObject").setMutable(false).setAccessible(true);
-
-		javaClass.addMethod()
-				.setConstructor(true)
-				.setPublic()
-				.setBody("this.builtObject = new " + currentClass.getSimpleName() + "();");
-
-		javaClass.addMethod()
-				.setPublic()
-				.setStatic(true)
-				.setName("Builder")
-				.setReturnType(javaClass.getName())
-				.setBody("return new " + javaClass.getName() + "();");
-
+		
+		// create builtObject
+		SourceCodeField builtObjectField = SourceCodeFieldBuilder.Builder(currentClass, "builtObject")
+																 .withIsFinal(true)
+																 .withFieldAccessLevel(FieldAccessLevel.PRIVATE)
+																 .build();
+		sourceCodeFields.add(builtObjectField);
+		
+		// create constructor
+		SourceCodeMethod constructor = SourceCodeMethodBuilder.Builder(MethodAccessLevel.PRIVATE, false, className, "this.builtObject = new " + currentClass.getSimpleName() + "();")
+															  .build();
+		sourceCodeMethods.add(constructor);
+		
+		// create static builder method
+		SourceCodeMethod staticBuilderMethod = SourceCodeMethodBuilder.Builder(MethodAccessLevel.PUBLIC, true, "Builder", "return new " + className + "();")
+																	  .withMethodReturnSelf()
+																	  .build();
+		sourceCodeMethods.add(staticBuilderMethod);
+		
+		// create withMethods for each field in class
 		Stream<Field> fields = Arrays.stream(currentClass.getDeclaredFields());
 		fields.forEach(field -> {
+			
 			String fieldType = field.getType().getSimpleName();
 			String fieldName = field.getName().substring(0, 1).toUpperCase() + field.getName().substring(1);
+			
 			if (fieldType.contains("List")) {
 
-				String listWithType = field.getGenericType().toString().replace("$", ".");
-				String listTypeFullyQualified = listWithType.substring(	listWithType.indexOf("<") + 1,
-																		listWithType.indexOf(">"));
-				String listType = listTypeFullyQualified.substring(listTypeFullyQualified.lastIndexOf(".") + 1);
-				javaClass.addImport(listTypeFullyQualified);
-
-				javaClass.addMethod()
-						.setName("with" + fieldName)
-						.setPublic()
-						.setReturnType(javaClass.getName())
-						.setBody("builtObject.get" + fieldName + "().add(" + field.getName() + ");\n return this;")
-						.addParameter(listType, field.getName());
-
+				ParameterizedType listType = (ParameterizedType) field.getGenericType();
+				
+				Type listGenericType = listType.getActualTypeArguments()[0];
+				
+				Class<?> listTypeGenericClass = null;
+				if(listGenericType.toString().contains("<")) {
+					
+					try {
+						listTypeGenericClass = Class.forName(listGenericType.getTypeName().substring(0, listGenericType.getTypeName().indexOf("<")));
+					} catch (ClassNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+				} else {
+				
+					listTypeGenericClass = (Class<?>) listType.getActualTypeArguments()[0];
+				
+				}
+		        
+				SourceCodeMethod withMethod = SourceCodeMethodBuilder.Builder(MethodAccessLevel.PUBLIC, false, "with" + fieldName, "builtObject.get" + fieldName + "().add(" + field.getName() + ");\n return this;")
+																	 .withMethodReturnSelf()
+																	 .withParameter(listTypeGenericClass, field.getName())
+																	 .build();
+				sourceCodeMethods.add(withMethod);
+				
 			}
 			else {
 
-				String fieldTypeImport = field.getType().getName().replace("$", ".");
-
-				javaClass.addImport(fieldTypeImport);
-
-				javaClass.addMethod()
-						.setName("with" + fieldName)
-						.setPublic()
-						.setReturnType(javaClass.getName())
-						.setBody("builtObject.set" + fieldName + "(" + field.getName() + ");\n return this;")
-						.addParameter(fieldType, field.getName());
-
+				SourceCodeMethod withMethod = SourceCodeMethodBuilder.Builder(MethodAccessLevel.PUBLIC, false, "with" + fieldName, "builtObject.set" + fieldName + "(" + field.getName() + ");\n return this;")
+						                                             .withMethodReturnSelf()
+																     .withParameter(field.getType(), field.getName())
+						                                             .build();
+				sourceCodeMethods.add(withMethod);
 			}
+			
 		});
+		
+		SourceCode sourceCode = SourceCodeBuilder.Builder(className, packageName)
+											     .withSourceCodeFields(sourceCodeFields)
+												 .withSourceCodeMethods(sourceCodeMethods)
+												 .build();
 
-		builders.put(javaClass.getPackage().replace(".", "/") + "/" + javaClass.getName(), javaClass.toString());
+
+		builders.put(packageName.replace(".", "/") + "/" + className, sourceCode.getFormattedSourceCode());
 
 		currentClass.getDeclaredClasses();
 		Stream<Class<?>> innerClasses = Arrays.stream(currentClass.getDeclaredClasses());
